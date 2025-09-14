b2ef985eb94ab47cba788b780479bdf766d0943c
Илья <ikrapivcev@gmail.com>
2025-09-14 20:22:39 +0300

    Готовое ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,66 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        paper = paper + 4;
+        System.out.println(paper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+
+
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend / 7;
+        System.out.println(friend);
+
+
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog * 10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+
+        var boxer1 = 78.2;
+        System.out.println(boxer1);
+        var boxer2 = 82.7;
+        System.out.println(boxer2);
+        var weightOfBoxers = boxer1 + boxer2;
+        System.out.println("Общий вес боксёров " + weightOfBoxers + " кг ");
+        var differenceWeight = boxer2 - boxer1;
+        System.out.println("Разница в весе " + differenceWeight);
+        var remains = boxer2 % boxer1;
+        System.out.println(remains);
+
+        var openingHours = 640;
+        var oneEmployee = 8;
+        var staff = openingHours / oneEmployee;
+        System.out.println("Всего сотрудников " + staff + " Человек ");
+
+        var newEmployees = staff + 94;
+        var hoursBetweenEmployees = oneEmployee * newEmployees;
+        System.out.println(" Если в компании работает " + newEmployees + " человека, то всего " + hoursBetweenEmployees + " часа работы может быть поделено между сотрудниками ");
+
+
 
         for (int i = 1; i <= 5; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
